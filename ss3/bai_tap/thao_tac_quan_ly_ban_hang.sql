3453c462ceaab4deba44196161466f28619e1684
cmtin1807 <cmtin1807@gmail.com>
2024-07-19 08:09:24 +0700

    hi7

--- ss3/bai_tap/thao_tac_quan_ly_ban_hang.sql
+++ ss3/bai_tap/thao_tac_quan_ly_ban_hang.sql
@@ -21,3 +21,23 @@ VALUES (1,1,3),
        (3,1,8),
        (2,5,4),
        (2,3,3);
+SELECT order_id AS oID, order_date AS oDate, order_total_price AS oPrice
+FROM orders;
+
+SELECT c.customer_id, c.customer_name, p.product_id, p.product_name
+FROM customer c
+         LEFT JOIN orders o ON c.customer_id = o.customer_id
+         LEFT JOIN order_detail od ON o.order_id = od.order_id
+         LEFT JOIN product p ON od.product_id = p.product_id;
+
+SELECT c.customer_id, c.customer_name
+FROM customer c
+         LEFT JOIN orders o ON c.customer_id = o.customer_id
+WHERE o.order_id IS NULL;
+
+SELECT o.order_id AS MaHoaDon, o.order_date AS NgayBan,
+       SUM(od.order_detail_qty * p.product_price) AS GiaTien
+FROM orders o
+         JOIN order_detail od ON o.order_id = od.order_id
+         JOIN product p ON od.product_id = p.product_id
+GROUP BY o.order_id, o.order_date;
